a061d5c33973abfd89d777d18591933d41ed733a
Julio Cesar <a20203521@pucp.edu.pe>
2024-06-19 19:23:02 -0500

    primer commint

---
+++ Lab9_20203521_GTICS/src/main/java/com/example/lab9_20203521_gtics/Lab920203521GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20203521_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab920203521GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab920203521GticsApplication.class, args);
+    }
+
+}
